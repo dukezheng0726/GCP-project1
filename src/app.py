6ea623a9bd8cd89d65e21a15afd53329ec7b4256
dukezheng0726 <duke.zheng0726@gmail.com>
2025-03-31 18:41:30 -0400

    0331

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "This is Yan's GCP project Demo!!(HTTS)"
+    return "This is Yan's GCP project Demo!!(HTTP)"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
